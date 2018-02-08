1c8f6adea08451544594e13cefc53a8af821cda2
wangyj <wangyj@liepin.com>
2018-02-08 15:36:26 +0800

    add MegaCliEnclosureInfo

---
+++ models/MegaCliEnclosureInfo.go
@@ -0,0 +1,7 @@
+package models
+
+// MegaCliEnclosureInfo ..
+type MegaCliEnclosureInfo struct {
+	EnclosureDeviceId      string `json:"enclosure_deviceid"`
+	NumberOfPhysicalDrives string `json:"number_of_physical_drives"`
+}
