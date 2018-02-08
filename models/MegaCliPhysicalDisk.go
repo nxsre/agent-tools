1ed9edf81ecc30ff2f4dac8a1a01ff5685dc3835
wangyj <wangyj@liepin.com>
2018-02-08 16:13:47 +0800

    add Predictive Failure Count

--- models/MegaCliPhysicalDisk.go
+++ models/MegaCliPhysicalDisk.go
@@ -2,17 +2,19 @@ package models
 
 // MegaCliPhysicalDisk ..
 type MegaCliPhysicalDisk struct {
-	EncDeviceId   int    `json:"enclosure_device_id"`
-	SlotNumber    int    `json:"slot_number"`
-	Wwn           string `json:"wwn"`
-	MedErrCount   string `json:"media_error_count"`
-	OthErrCount   string `json:"other_error_count"`
-	PdType        string `json:"pd_type"`
-	RawSize       string `json:"raw_size"`
-	FirmwareState string `json:"firmware_state"`
-	InquiryData   string `json:"inquiry_data"`
-	DeviceSpeed   string `json:"device_speed"`
-	LinkSpeed     string `json:"link_speed"`
-	MediaType     string `json:"media_type"`
-	DriveTemp     string `json:"drive_temp"`
+	EncDeviceId                         int    `json:"enclosure_device_id"`
+	SlotNumber                          int    `json:"slot_number"`
+	Wwn                                 string `json:"wwn"`
+	MedErrCount                         string `json:"media_error_count"`
+	OthErrCount                         string `json:"other_error_count"`
+	PredictiveFailureCount              string `json:"predictive_failure_ount"`
+	LastPredictiveFailureEventSeqNumber string `json:"last_predictive_failure_event_seq_number"`
+	PdType                              string `json:"pd_type"`
+	RawSize                             string `json:"raw_size"`
+	FirmwareState                       string `json:"firmware_state"`
+	InquiryData                         string `json:"inquiry_data"`
+	DeviceSpeed                         string `json:"device_speed"`
+	LinkSpeed                           string `json:"link_speed"`
+	MediaType                           string `json:"media_type"`
+	DriveTemp                           string `json:"drive_temp"`
 }
